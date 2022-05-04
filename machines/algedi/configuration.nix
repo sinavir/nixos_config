12471dbd1a17c810351ec992f30a5f8b2a9b9a07
Maurice Debray <mdebray@clipper.ens.fr>
2022-05-04 17:18:51 +0200

    ~

--- machines/algedi/configuration.nix
+++ machines/algedi/configuration.nix
@@ -9,33 +9,23 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../shared/users.nix
-      ./syncthing.nix
-      ./nat.nix
-      # ./git.nix
-      ./kfet2mqtt.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.systemd-boot.enable = true;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
-
   networking.hostName = "algedi"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.eno1.useDHCP = true;
+
+  # Ici la config des IPs
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
