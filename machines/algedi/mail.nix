adb3b4958567537b93600307acc1052c2e2535d5
Maurice Debray <mdebray@clipper.ens.fr>
2022-05-14 10:10:21 +0200

    mail

---
+++ machines/algedi/mail.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    (builtins.fetchTarball {
+      # Pick a commit from the branch you are interested in
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/6e3a7b2ea6f0d68b82027b988aa25d3423787303/nixos-mailserver-6e3a7b2ea6f0d68b82027b988aa25d3423787303.tar.gz";
+      # And set its hash
+      sha256 = "d6c8ed758925663bc77559f6845ee8034b6a1c4903c3881e6e8d065314b14bfc";
+    })
+  ];
+
+  mailserver = {
+    enable = true;
+    fqdn = "mail.sinavir.fr";
+    domains = [ "sinavir.fr" ];
+
+    # A list of all login accounts. To create the password hashes, use
+    # nix run nixpkgs.apacheHttpd -c htpasswd -nbB "" "super secret password" | cut -d: -f2
+    loginAccounts = {
+      "sinavir@sinavir.fr" = {
+         hashedPasswordFile = "/var/lib/mail/sinavir";
+         aliases = [
+           "maurice@sinavir.fr"
+           "contact@sinavir.fr"
+           "@sinavir.fr"
+         ];
+         quota="5G";
+       };
+    };
+
+    # Use Let's Encrypt certificates. Note that this needs to set up a stripped
+    # down nginx and opens port 80.
+    certificateScheme = 3;
+  };
+}
