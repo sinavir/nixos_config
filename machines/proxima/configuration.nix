59104decedfd70ef06da7580a6904a6faff7b534
Maurice Debray <mdebray@clipper.ens.fr>
2022-05-15 03:50:36 +0200

    ~

--- machines/proxima/configuration.nix
+++ machines/proxima/configuration.nix
@@ -18,7 +18,9 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.efiInstallAsRemovable = true;
 
   me.lan.ipv4 = "10.100.0.1";
   me.lan.prefixSize4 = 24;
