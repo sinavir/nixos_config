3793a54e4080a4dc8348bc92c365d35443a38fdc
sinavir <sinavir@sinavir.fr>
2022-06-07 15:22:57 +0200

    ~

--- shared/syncthing.nix
+++ shared/syncthing.nix
@@ -3,7 +3,7 @@
   services.syncthing = rec {
     enable = true;
     devices.honor.id = "K5IKPPP-MTGTVAO-A3M2W6H-KY3GLR6-QKTOK3O-OYIA6GX-YVOOJQ5-NN4GLQA";
-    devices.polaris.id = "3QBL655-PJILD3U-MOIFIJM-ZBS4EH4-5AIVYLK-RMCNYI7-3FHRMFP-VFD6CQE";
+    devices.polaris.id = "YHPG5F4-YSL6GOL-LUVLAST-46BRVNH-LO7P5KF-IZSWBIY-R4G3QZ5-YJPGIQ3";
     devices.mintaka.id = "QZ3QBKX-YTHW5RB-T33URQX-L77UXW4-Q344EPF-RRC3Y27-EHVBNN4-7RGRPAJ";
     folders = {
       pwd = {
