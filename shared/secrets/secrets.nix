fff8a125f9758dc514e69f9926e235763a241c1b
sinavir <contact@sinavir.fr>
2022-06-15 15:56:09 +0200

    wg algedi avec age

--- shared/secrets/secrets.nix
+++ shared/secrets/secrets.nix
@@ -3,5 +3,6 @@ let
   readPubkeys = user: builtins.filter (k: k != "") (lib.splitString "\n" (builtins.readFile (../pubkeys + "/${user}.keys")));
 in
 {
+  "wg-algedi.age".publicKeys = (readPubkeys "maurice") ++ (readPubkeys "algedi");
   "wg-polaris.age".publicKeys = (readPubkeys "maurice");
 }
