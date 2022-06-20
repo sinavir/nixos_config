78dfc310647fa5fdc40f5a1f8dd35c2c71cb0c3b
sinavir <sinavir@sinavir.fr>
2022-06-21 00:13:43 +0200

    ~

--- machines/elnath/keycloak.nix
+++ machines/elnath/keycloak.nix
@@ -17,8 +17,8 @@
     enable = true;
     recommendedProxySettings = true;
     virtualHosts."192.168.1.167" = {
-      listen = [{ addr = "192.168.1.167"; port = 443; }];
-      forceSSL = true;
+      listen = [{ addr = "192.168.1.167"; port = 443; ssl = true; }];
+      addSSL = true;
       sslCertificate = ../../shared/hackens-lan-167.crt;
       sslCertificateKey = config.age.secrets.hackensKey.path;
       locations."/" = {
@@ -26,8 +26,8 @@
       };
     };
     virtualHosts."10.100.1.5" = {
-      listen = [{ addr = "10.100.1.5"; port = 443; }];
-      forceSSL = true;
+      listen = [{ addr = "10.100.1.5"; port = 443; ssl = true; }];
+      addSSL = true;
       sslCertificate = ../../shared/maurice-vpn-005.crt;
       sslCertificateKey = config.age.secrets.mauriceVpnKey.path;
       locations."/" = {
