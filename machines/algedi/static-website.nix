a95cd9b053af88d03106b83ed2e83f21d1b70c7c
sinavir <contact@sinavir.fr>
2022-08-13 18:03:55 +0200

    Sat Aug 13 06:03:55 PM CEST 2022

--- machines/algedi/static-website.nix
+++ machines/algedi/static-website.nix
@@ -2,10 +2,17 @@
 {
   services.nginx = {
     enable = true;
-    virtualHosts."rz.sinavir.fr" = {
+    virtualHosts."10.100.1.2" = {
       default = true;
       root = "/var/lib/proxy.sinavir.fr";
       listen = [ {addr = "10.100.1.2"; port = 80; } ];
+      locations = {
+        "/rz/76127651-CD_vashfol/" = {
+          alias = "/var/lib/proxy.sinavir.fr/76127651-CD_fanf/";
+          extraConfig = "autoindex on;";
+        };
+      };
     };
   };
+  networking.firewall.interfaces.wg0.allowedTCPPorts = [ 80 ];
 }
