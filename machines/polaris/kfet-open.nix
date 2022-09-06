8b97aec6714becff34210ed170dee2556a463d66
sinavir <sinavir@sinavir.fr>
2022-09-07 01:14:54 +0200

    Wed Sep  7 01:14:54 AM CEST 2022

--- machines/polaris/kfet-open.nix
+++ machines/polaris/kfet-open.nix
@@ -1,10 +1,13 @@
 { pkgs, config, lib, ... }:
+let
+  rwebsocat = pkgs.python310Packages.callPackage ./rwebsocat.nix {};
+in
 {
   systemd.user.services.kfetIndicator = {
     after = [ "network.target" ];
     description = "K-Fêt indicator";
     script = ''
-      ${pkgs.websocat}/bin/websocat -vvv --ping-interval 5 --ping-timeout 15 --no-close wss://cof.ens.fr/ws/k-fet/open \
+      ${rwebsocat}/bin/rwebsocat wss://cof.ens.fr/ws/k-fet/open \
       | ${pkgs.jq}/bin/jq --unbuffered -Rr 'fromjson? | .status?' \
       | sed -u 's/closed/🔴 Close/; s/unknown/🟠 Unknown/; s/opened/🟢 Open/' \
       | xargs -I message ${pkgs.dbus}/bin/dbus-send --dest=i3.status.rs --type=method_call /isKFetOpen i3.status.rs.SetStatus 'string:🍺 message'
