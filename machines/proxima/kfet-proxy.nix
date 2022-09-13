85273053b2ae0250f0c08a5a0a20aad45b414df3
sinavir <sinavir@sinavir.fr>
2022-09-13 16:40:28 +0200

    Tue Sep 13 04:40:28 PM CEST 2022

--- machines/proxima/kfet-proxy.nix
+++ machines/proxima/kfet-proxy.nix
@@ -2,6 +2,8 @@
 {
   services.nginx = {
     virtualhosts."kfet.sinavir.fr" = {
+      enableACME = true;
+      forceSSL = true;
       root = ./kfet-proxy-src;
       locations."ws/" = {
         proxyPass = "https://cof.ens.fr/ws/k-fet/open/";
