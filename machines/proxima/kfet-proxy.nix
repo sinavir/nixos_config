c80dcb6ded4949fba37f85b979aadee2cc6e13ef
sinavir <sinavir@sinavir.fr>
2022-09-19 08:59:28 +0200

    Mon Sep 19 08:59:28 AM CEST 2022

--- machines/proxima/kfet-proxy.nix
+++ machines/proxima/kfet-proxy.nix
@@ -5,9 +5,12 @@
       enableACME = true;
       forceSSL = true;
       root = ./kfet-proxy-src;
-      locations."ws/" = {
+      locations."/ws/" = {
         proxyPass = "https://cof.ens.fr/ws/k-fet/open/";
         proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host cof.ens.fr;
+        '';
       };
     };
   };
