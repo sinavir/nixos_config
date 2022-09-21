f76ec1f0a866d08df46e1292f523bd1382f70928
sinavir <sinavir@sinavir.fr>
2022-09-21 21:09:20 +0200

    Wed Sep 21 09:09:20 PM CEST 2022

--- machines/proxima/configuration.nix
+++ machines/proxima/configuration.nix
@@ -75,6 +75,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    screen
     htop
     (pkgs.callPackage <agenix/pkgs/agenix.nix> {})
   ];
