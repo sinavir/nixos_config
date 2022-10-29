ad049310a585316d95f9b2e209dbbaa7055e3cac
sinavir <sinavir@sinavir.fr>
2022-10-29 21:29:40 +0200

    Sat Oct 29 09:29:40 PM CEST 2022

--- machines/proxima/wireguard.nix
+++ machines/proxima/wireguard.nix
@@ -12,21 +12,10 @@
         config.shared.wg.peers.algedi
         config.shared.wg.peers.polaris
         config.shared.wg.peers.mintaka
-        config.shared.wg.peers.elnath
-      ];
-    };
-    wg-led = {
-      # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = [ "10.100.5.2/24" ];
-      listenPort = 51845;
-      privateKeyFile = config.age.secrets."wg-led-proxima".path;
-      peers = [
-        config.shared.wg.peers.ap-unifi
       ];
     };
   };
-  networking.firewall.trustedInterfaces = [ "wg-led" ];
   networking.firewall = {
-    allowedUDPPorts = [ 51820 51845 ]; # Clients and peers can use the same port, see listenport
+    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
   };
 }
