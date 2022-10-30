559731c9e59b478a4387dbea571894fb9d361685
sinavir <sinavir@sinavir.fr>
2022-10-30 22:54:34 +0100

    Sun Oct 30 10:54:34 PM CET 2022

--- machines/proxima/ws-scraper/ws-scraper.nix
+++ machines/proxima/ws-scraper/ws-scraper.nix
@@ -6,8 +6,8 @@ buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "sinavir";
     repo = "ws-scraper";
-    rev = "d9f19ea6b3e7efd5bb06e23c693e303e4c22de0c";
-    sha256 = "sha256-9BN+p96Lllu27zE+yRSmD3rTnA+ST/uBXKTh6VKnzmI=";
+    rev = "b719f5791914cf1a57e525a8e8d18b2585c7bea7";
+    sha256 = "sha256-5RwqPAcfRpUP0dgF/rTJb1974+tkNarjZ5//nOwmKe8=";
   };
   propagatedBuildInputs = builtins.trace "${aiosqlite}" [ aiosqlite websockets ];
 }
