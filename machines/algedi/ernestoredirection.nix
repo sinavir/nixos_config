b4a4670dce3d77a07c767997e36963e7328833c3
sinavir <contact@sinavir.fr>
2022-12-25 02:24:43 +0100

    ernestostuff

--- machines/algedi/ernestoredirection.nix
+++ machines/algedi/ernestoredirection.nix
@@ -5,6 +5,12 @@
       enableACME = true;
       addSSL = true;
       locations."/".return = "302 https://ernestophone.ens.fr";
+      locations."/kalo.mp3" = {
+        root = ./kalo;
+      };
+      locations."/files" = {
+        root = ./files;
+      };
     };
     virtualHosts."ernestoburo.ernestophone.fr" = {
       enableACME = true;
