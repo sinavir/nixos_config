d10011f6d47f3a72a0f2b07f68a415225343e994
sinavir <sinavir@sinavir.fr>
2023-02-14 02:37:58 +0100

    systemd-networkd

--- machines/algedi/networking.nix
+++ machines/algedi/networking.nix
@@ -2,16 +2,15 @@
 {
   networking.hostName = "algedi"; # Define your hostname.
 
-  networking.useDHCP = false;
-  networking.interfaces.ens18 = {
-    useDHCP = true;
-    ipv6 = {
-      addresses = [{
-        address = "2001:470:1f13:187:b256:8cb7:beb0:9d45";
-        prefixLength = 64;
-      }];
+  systemd.networking = {
+    enable = true;
+    networks = {
+      "ipv6-uplink" = {
+        name = "ens18";
+        address = "2001:470:1f13:187:b256:8cb7:beb0:9d45/64";
+        networkConfig.MTUBytes = "1350";
+      };
     };
-    mtu = 1350;
   };
   networking.interfaces.ens21 = {
     useDHCP = false;
