63d89f93b6e11f3db0605ead81833b2ad4c6a5e1
sinavir <sinavir@sinavir.fr>
2023-02-14 14:44:12 +0100

    systemd-network on proxima

--- machines/proxima/networking.nix
+++ machines/proxima/networking.nix
@@ -1,19 +1,18 @@
 { config, pkgs, lib, ... }:
 {
   networking.useDHCP = false;
-  networking.interfaces.ens3 = {
-    useDHCP = true;
-    ipv6.addresses = [{
-      address = "2001:41d0:404:200::81a1";
-      prefixLength = 128;
-    }];
-    ipv4.addresses = [{
-      address = "51.210.243.54";
-      prefixLength = 24;
-    }];
-  };
-  networking.defaultGateway6 = {
-    address = "2001:41d0:404:200::1";
-    interface = "ens3";
+  systemd.network = {
+    enable = true;
+    networks = {
+      "uplink" = {
+        name = "ens3";
+        DHCP = "ipv4";
+        address = [
+          "2001:41d0:404:200::81a1/64"
+          "51.210.243.54/32"
+        ];
+        networkConfig = { Gateway = "2001:41d0:404:200::1"; };
+      };
+    };
   };
 }
