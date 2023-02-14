81ec5334b27ddafcde5a753791baf1937631f8d7
sinavir <sinavir@sinavir.fr>
2023-02-14 14:41:11 +0100

    password for root

--- shared/users.nix
+++ shared/users.nix
@@ -1,4 +1,7 @@
 { ... }: {
   users.mutableUsers = false;
-  users.users.root.openssh.authorizedKeys.keyFiles = [ ./pubkeys/maurice.keys ./pubkeys/honor.keys ];
+  users.users.root = {
+    openssh.authorizedKeys.keyFiles = [ ./pubkeys/maurice.keys ./pubkeys/honor.keys ];
+    hashedPassword = "$6$BlPEVPOFNnvfTvLQ$o/GtstzjETp93MnTpEiYG664Einj4Ow7TQHi5ODv0gNhZdbWKhnQKai1eyBwUw/1bufx9SHd2RM9k1O3XjtNb.";
+  };
 }
