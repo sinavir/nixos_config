2bbd05097242dd4ceccbec1eb1be47fc12efa947
sinavir <contact@sinavir.fr>
2023-02-14 13:55:48 +0100

    ~

--- machines/algedi/networking.nix
+++ machines/algedi/networking.nix
@@ -1,6 +1,7 @@
 { pkgs, config, lib, ... }:
 {
   networking.hostName = "algedi"; # Define your hostname.
+  networking.useDHCP = false;
 
   systemd.network = {
     enable = true;
@@ -10,25 +11,19 @@
         address = [ "2001:470:1f13:187:b256:8cb7:beb0:9d45/64" ];
         linkConfig.MTUBytes = "1350";
       };
-    };
-  };
-  networking.interfaces.ens21 = {
-    useDHCP = false;
-    ipv4 = {
-      addresses = [{
-        address = "45.13.104.28";
-        prefixLength = 32;
-      }];
-      routes = [{
-        address = "0.0.0.0";
-        prefixLength = 0;
-      }];
+      "ipv4-uplink" = {
+        name = "ens21";
+        address = [ "45.13.104.28/32" ];
+        networkConfig = {
+          DefaultRouteOnDevice = true;
+        };
+      };
     };
   };
   networking.nameservers = [
-    "9.9.9.9"
-    "149.112.112.112"
     "2620:fe::fe"
     "2620:fe::9"
+    "9.9.9.9"
+    "149.112.112.112"
   ];
 }
