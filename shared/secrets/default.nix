b8e1ecf51ff0c06b910eda847b8f7074b6582669
sinavir <contact@sinavir.fr>
2023-02-16 17:43:42 +0100

    monitoring improvment

--- shared/secrets/default.nix
+++ shared/secrets/default.nix
@@ -1,6 +1,11 @@
 { pkgs, config, lib, ... }: {
   imports = [ <agenix/modules/age.nix> ];
   age.secrets = {
-    "ntfy-passwd".file = ./ntfy.age;
+    "ntfy-passwd" = {
+      file = ./ntfy.age;
+      group = "ntfy-access";
+      mode = "0440";
+    };
   };
+  users.groups."ntfy-access" = {};
 }
