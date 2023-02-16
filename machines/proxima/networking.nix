727e555a9de5a3a98600a3efdc3174932e32a6fe
sinavir <sinavir@sinavir.fr>
2023-02-16 05:48:51 +0100

    /root

--- machines/proxima/networking.nix
+++ machines/proxima/networking.nix
@@ -21,7 +21,7 @@ in
         };
         wireguardPeers = lib.mapAttrsToList (peer: conf: { wireguardPeerConfig = {
           inherit (conf) Endpoint PublicKey;
-          AllowedIPs = conf.defaultAllowedIPs ++ if peer == "algedi" then [ "::/0" ] else [];
+          AllowedIPs = conf.defaultAllowedIPs;
         };}) peers;
       };
     };
