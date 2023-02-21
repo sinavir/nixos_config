d35f2a867481ea9a79218a21258ffed4e7f38580
sinavir <sinavir@sinavir.fr>
2023-02-21 15:46:59 +0100

    monitor systemd

--- machines/proxima/monitoring.nix
+++ machines/proxima/monitoring.nix
@@ -12,10 +12,15 @@
     };
     configDir = {
       "stream.conf" = pkgs.writeText "stream.conf" ''
-                  [stream]
-                    enabled = yes
-        	    destination = 10.100.1.2:19999
-        	    api key = eb9e2ae9-a6ae-4737-85d7-aac5fabf8bcf
+        [stream]
+          enabled = yes
+          destination = 10.100.1.2:19999
+          api key = eb9e2ae9-a6ae-4737-85d7-aac5fabf8bcf
+      '';
+      "go.d.conf" = pkgs.writeText "go.d.conf" ''
+        # go.d.conf
+        modules:
+          systemdunits: yes
       '';
     };
 
