9d509c3e610af3422778bbf8f564db7af5842504
sinavir <sinavir@sinavir.fr>
2023-04-22 07:32:45 +0200

    keycloak working

--- machines/proxima/keycloak.nix
+++ machines/proxima/keycloak.nix
@@ -22,6 +22,13 @@ in
       enableACME = true;
       locations."/" = {
         proxyPass = "http://localhost:${builtins.toString port}";
+        extraConfig = ''
+          # For large authentication-authorization headers
+          proxy_buffer_size          128k;
+          proxy_buffers              4 256k;
+          proxy_busy_buffers_size    256k;
+        '';
+
       };
     };
   };
