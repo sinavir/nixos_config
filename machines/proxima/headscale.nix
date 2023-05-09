0a3d237d509d69508c47b10c0108b0c886385a8d
sinavir <sinavir@sinavir.fr>
2023-05-09 12:31:58 +0200

    ~

--- machines/proxima/headscale.nix
+++ machines/proxima/headscale.nix
@@ -5,19 +5,11 @@
     port = 8085;
     address = "127.0.0.1";
     settings = {
-      dns_config = {
-        override_local_dns = true;
-        base_domain = "";
-        magic_dns = true;
-        nameservers = [
-          "1.1.1.1"
-        ];
-      };
       log.level = "debug";
       server_url = "https://vpn.sinavir.fr";
       #metrics_listen_addr = "127.0.0.1:8095";
       ip_prefixes = [
-        "10.111.0.0/16"
+        "100.64.0.0/10"
       ];
       oidc = {
         issuer = "https://auth.sinavir.fr";
