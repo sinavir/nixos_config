469575dc4b1fd624ec85a575da9e1598a457cd96
sinavir <sinavir@sinavir.fr>
2023-05-10 23:51:00 +0200

    ~

--- machines/capella/networking.nix
+++ machines/capella/networking.nix
@@ -28,4 +28,5 @@
     requires = [ "tailscaled.service" ];
     script = "${config.services.tailscale.package}/bin/tailscale up --login-server https://vpn.sinavir.fr --auth-key file:${config.age.secrets."vpn_preauth".path}";
   };
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
 }
