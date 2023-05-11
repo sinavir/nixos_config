cd27fb5bab2e38f4b29659620f45d4ac11e8dc79
sinavir <sinavir@sinavir.fr>
2023-05-11 09:49:56 +0200

    capella:noleds

--- machines/capella/configuration.nix
+++ machines/capella/configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./noleds.nix
     ./bootloader.nix
 ./secrets
     ../../shared
---
+++ machines/capella/noleds.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  system.activationScripts.noled.text = ''
+  echo 0 > /sys/devices/platform/leds/leds/PWR/brightness
+  echo 0 > /sys/devices/platform/leds/leds/ACT/brightness
+  '';
+}
